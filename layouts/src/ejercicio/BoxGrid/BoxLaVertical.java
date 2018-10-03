aecb0c3f9132bdb71e54693dc721ffee89b8f8c9
likendero <likendero11@gmail.com>
2018-10-03 10:16:01 +0200

    inicio tarea box y grid

---
+++ layouts/src/ejercicio/BoxGrid/BoxLaVertical.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.BoxGrid;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author linuxdiurno
+ */
+public class BoxLaVertical {
+    
+}
+/**
+ * ventana
+ * @author linuxdiurno
+ */
+class VentanaVertical extends JFrame{
+
+}
+/**
+ * panel
+ * @author linuxdiurno
+ */
+class PanelVertical extends JPanel{
+    public PanelVertical(){
+    
+    }
+}
