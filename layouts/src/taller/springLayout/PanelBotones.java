b5cd8226735d9249254007fe6f3dd951f194d39d
likendero <likendero11@gmail.com>
2018-10-18 14:35:25 +0200

    cosas

--- layouts/src/taller/springLayout/PanelBotones.java
+++ layouts/src/taller/springLayout/PanelBotones.java
@@ -5,13 +5,70 @@
  */
 package taller.springLayout;
 
+import java.awt.Container;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.Spring;
+import javax.swing.SpringLayout;
+
 /**
- *
- * @author linuxdiurno
+ *  clase que crea un panel con botones, este 
+ * utiliza un Spring layout
+ * @author Javier Gonzalez Rives
  */
-public class PanelBotones {
-    
-    public PanelBotones(){
-    
+public class PanelBotones extends JPanel {
+    private JButton bt1,bt2,bt3,bt4;
+    private SpringLayout ly;
+    private Container cont;
+    /**
+     * metodo constructor del panel con botones
+     */
+    public PanelBotones(Container cont){
+        super();
+        this.cont = cont;
+        init();
+    }
+    /**
+     * metodo que inicializa los elementos del panel
+     * tambien cambia el layout
+     */
+    private void init(){
+        // inicializacion de los botones
+        bt1 = new JButton("boton1");
+        bt2 = new JButton("boton2");
+        bt3 = new JButton("boton3");
+        bt4 = new JButton("boton4");
+        
+        // creacion del layout
+        ly = new SpringLayout();
+        setLayout(ly);
+        // adds
+        add(bt1);
+        add(bt2);
+        add(bt3);
+        //add(bt4);
+        
+        // restricciones
+        springs();
+    }
+    /**
+     * metodo que define la posicion de los 
+     * elementos
+     */
+    public void springs(){
+        // enganchado a 10 de la parte superior de la ventana
+        ly.putConstraint(SpringLayout.NORTH, bt1, 10, SpringLayout.NORTH, cont);
+        // enganchado a 10 de la parte izquierda
+        ly.putConstraint(SpringLayout.WEST, bt1, 10, SpringLayout.WEST, cont);
+        
+        // prueba con Springs
+        Spring muelle = Spring.constant(50, 80, 100);
+        ly.putConstraint(SpringLayout.WEST, bt2, muelle, SpringLayout.EAST, bt1);
+        ly.putConstraint(SpringLayout.NORTH, bt2, 10, SpringLayout.NORTH, cont);
+        //ly.putConstraint(SpringLayout.EAST, bt2, muelle, SpringLayout.WEST, bt3);
+        
+        // bt3
+        ly.putConstraint(SpringLayout.NORTH, bt3, 10, SpringLayout.NORTH, cont);
+        ly.putConstraint(SpringLayout.EAST, bt3, muelle, SpringLayout.EAST, cont);
     }
 }
--- layouts/src/taller/springLayout/Principal.java
+++ layouts/src/taller/springLayout/Principal.java
@@ -1,10 +1,33 @@
 
 package taller.springLayout;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 /**
  *
  * @author linuxdiurno
  */
 public class Principal {
     
+    /**
+     * metodo que crea una ventana y despues la muestra
+     */
+    public static void  mostrar(){
+        
+        JFrame ven = new JFrame("Spring");
+        PanelBotones panel = new PanelBotones(ven.getContentPane());
+        ven.setBounds(100, 100, 400, 400);
+        ven.setDefaultCloseOperation(ven.EXIT_ON_CLOSE);
+        ven.add(panel);
+        ven.setVisible(true);
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                mostrar();
+            }
+        });
+    }
 }
